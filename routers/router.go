c1cf89814d5047c49c12cba3dd7fb50ecb86f820
muhongwei <muhongwei@ghostcloud.cn>
2017-10-24 21:55:20 +0800

    modify ruter.go

--- routers/router.go
+++ routers/router.go
@@ -7,7 +7,6 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{})
-	beego.Router("/hello-world/:id([0-9]+)", &controllers.MainController{}, "get:HelloSitepoint")
+	beego.Router("/", &controllers.MainController{}, "get:HelloSitepoint")
 	beego.Router("/test", &controllers.MainController{}, "get:Test")
 }
