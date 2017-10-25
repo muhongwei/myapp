4b51cc990cfb0d833b4116c66d4002823e745a9b
muhongwei <muhongwei@ghostcloud.cn>
2017-10-25 18:51:32 +0800

    add helloserver to myapp

--- main.go
+++ main.go
@@ -3,10 +3,50 @@ package main
 import (
 	_ "myapp/routers"
 	"github.com/astaxie/beego"
+
+	"log"
+	"net"
+ 
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "helloworld/helloworld"
+
 )
 
+const (
+	port = ":50056"
+ )
+ 
+ type server struct {}
+ 
+ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message:in.Name}, nil
+ }
+ func startServer(){
+	 
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
+
+ }
+
 func main() {
+
+	
 	beego.BConfig.WebConfig.Session.SessionOn = true
+
+	//开启hello server
+	startServer()
+
 	beego.Run()
+
+	
+
+
+	
 }
 
