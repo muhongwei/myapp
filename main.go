fb76364561a1ab45793600f6f161f60eda0b408a
muhongwei <muhongwei@ghostcloud.cn>
2017-10-25 22:43:25 +0800

    add echo service

--- main.go
+++ main.go
@@ -6,30 +6,28 @@ import (
 
 	"log"
 	"net"
- 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "helloworld/helloworld"
+	pb "echo/echo"
 
 )
 
-const (
-	port = ":50056"
- )
- 
- type server struct {}
- 
- func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message:in.Name}, nil
- }
- func startServer(){
+const(
+	port=":50052"
+)
+type echoServer struct{}
+
+func(s *echoServer) Echo(ctx context.Context,in *pb.EchoRequest)(*pb.EchoReply,error){
+	return &pb.EchoReply{EplayMessage:in.RequestMessage},nil
+}
+ func startEchoServer(){
 	 
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatal("failed to listen: %v", err)
+	lis,err :=net.Listen("tcp",port)
+	if err != nil{
+		log.Fatal("failed to listen:%v",err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	s:=grpc.NewServer()
+	pb.RegisterEchoerServer(s,&echoServer{})
 	s.Serve(lis)
 
  }
@@ -40,7 +38,7 @@ func main() {
 	beego.BConfig.WebConfig.Session.SessionOn = true
 
 	//开启hello server
-	startServer()
+	startEchoServer()
 
 	beego.Run()
 
