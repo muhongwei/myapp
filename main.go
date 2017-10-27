4f62563e5c74fe9696b6c64a801137d00359a1f6
muhongwei <muhongwei@ghostcloud.cn>
2017-10-27 16:24:00 +0800

    update streamEchoService and baoshi

--- main.go
+++ main.go
@@ -7,45 +7,72 @@ import (
 
 	"log"
 	"net"
-	"golang.org/x/net/context"
+	"flag"
+	"time"
+	"io"
+	//"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	pb "echo/echo"
-	"google.golang.org/grpc/reflection"
+	pb "streamEcho/streamEcho"
+
+	//"google.golang.org/grpc/reflection"
 
 )
 
-const(
-	port=":50052"
+type sEchoServer struct{}
+var (
+	port       = flag.Int("port", 10009, "The server port")
 )
-type echoServer struct{}
+func (s *sEchoServer) Echo(stream pb.Echoer_EchoServer) error{
 
-func(s *echoServer) Echo(ctx context.Context,in *pb.EchoRequest)(*pb.EchoReply,error){
-	return &pb.EchoReply{EplayMessage:in.RequestMessage},nil
+	go func(){
+		ticker := time.NewTicker(10 * time.Second)
+		for i := 0; i < 10; i++ {
+			time := <-ticker.C
+			//fmt.Println(time.String())
+			var rPly pb.EchoReply
+			rPly.TimeNow = time.String()
+			fmt.Println(rPly.TimeNow)
+			stream.Send(&rPly)
+		}
+	}()
+
+	for{
+		in,err := stream.Recv()
+		fmt.Println(in)
+		if err == io.EOF{
+			fmt.Println("serverEOF")
+			return nil
+		}
+		if err != nil{
+			return err
+		}
+		var rPly pb.EchoReply
+		rPly.EplayMessage=in.RequestMessage
+		if err1 := stream.Send(&rPly);err1 != nil{
+			return err1
+		}
+
+
+	}
 }
- func startEchoServer(){
-	 
-	lis,err :=net.Listen("tcp",port)
+func startEchoServer(){
+	flag.Parse()
+	lis,err := net.Listen("tcp",fmt.Sprintf("localhost:%d",*port))
 	if err != nil{
 		log.Fatal("failed to listen:%v",err)
 	}
-	s:=grpc.NewServer()
-	pb.RegisterEchoerServer(s,&echoServer{})
-	//s.Serve(lis)
-	reflection.Register(s)
-	fmt.Println("before .serve(lis)")
-	if err :=s.Serve(lis);err != nil{
-		log.Fatalf("failed to server:%v",err)
-	}
-	fmt.Println("startover")
-
- }
+	streamEchoServer := grpc.NewServer()
+	s := new(sEchoServer)
+	pb.RegisterEchoerServer(streamEchoServer,s)
+	streamEchoServer.Serve(lis)
+}
 
 func main() {
 	
 	beego.BConfig.WebConfig.Session.SessionOn = true
 
 	//开启hello server
-	//startEchoServer()
+	go startEchoServer()
 
 	beego.Run()
 
