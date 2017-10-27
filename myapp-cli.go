4b5b326e7d68edb767a48e16de478bc67d4b45a3
muhongwei <muhongwei@ghostcloud.cn>
2017-10-27 16:29:30 +0800

    update myapp-cli

--- myapp-cli.go
+++ myapp-cli.go
@@ -1,52 +1,80 @@
-// package main
+package main
 
-// //echoClient.go
+//streamEchoClient.go
 
-// import(
-// 	"log"
-// 	"os"
-// 	"fmt"
+import(
+	"flag"
+	"log"
+	"os"
+	"fmt"
+	"io"
+	//"bufio"
 
-// 	"golang.org/x/net/context"
-// 	"google.golang.org/grpc"
-// 	pb "echo/echo"
-// )
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "streamEcho/streamEcho"
+)
 
-// const(
-// 	address ="localhost:50052"
-// )
-// func main(){
-// 	fmt.Println("输入exit退出!")
-// 	for{
-// 		conn,err := grpc.Dial(address,grpc.WithInsecure())
-// 		if err != nil{
-// 			log.Fatal("did not connect:%v",err)
-// 		}
-// 		defer conn.Close()
-// 		//c :=pb.NewEchoerServer(conn)
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile             = flag.String("ca_file", "", "The file containning the CA root cert file")
+	serverAddr         = flag.String("server_addr", "127.0.0.1:10009", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name use to verify the hostname returned by TLS handshake")
+)
+func runEcho(client pb.EchoerClient){
+	stream,err := client.Echo(context.Background())
+	if err != nil{
+		log.Fatalf("%v.Echo(_)= _,%v",client,err)
+	}
+	waitc := make(chan struct{})
+	go func(){
+		for{
+			in,err := stream.Recv()
+			if err == io.EOF{
+				close(waitc)
+				return
+			}
+			//fmt.Println(in)
+			if err != nil{
+				log.Fatalf("Failed to receive a note:%v",err)
+			}
+			//log.Print(in.EplayMessage)
+			//fmt.Print("message:")
+			//log.Printf("message:%s",in.EplayMessage)
+			fmt.Println(in.EplayMessage)
+			if in.TimeNow != ""{
+				fmt.Println(in.TimeNow)
+			}
+			
+			
+		}
+	}()
+	fmt.Println("输入exit退出：")
+	
+	for{
+		var note pb.EchoRequest
+		fmt.Scanf("%s\n",&note.RequestMessage)
+		if note.RequestMessage == "exit"{
+			os.Exit(0)
+		}
+		if err := stream.Send(&note);err !=nil {
+			log.Fatalf("failed to send a not:%v",err)
+		}		
+	}
+	stream.CloseSend()
+	<-waitc
 
-// 		//c :=pb.NewEchoerClient(conn)
-// 		c :=pb.NewEchoerClient(conn)
-		
-// 		fmt.Print("echo:")
-
-// 		var message string
-// 		if _,err := fmt.Scanf("%s\n",&message);err != nil{
-// 			fmt.Printf("%s\n",err)
-// 		}
-// 		if message == "exit"{
-// 			break
-// 		}
-
-// 		if len(os.Args) > 1{
-// 			message = os.Args[1]
-// 		}
-
-// 		//r,err := c.Echo(context.Background(),&pb.EchoRequest{EplayMessage:message})
-// 		r,err := c.Echo(context.Background(),&pb.EchoRequest{RequestMessage:message})
-// 		if err != nil{
-// 			log.Fatal("could not echo:%v",err)
-// 		}
-// 		fmt.Printf("%s\n",r.EplayMessage)
-// 	}
-// }
+}
+func main(){
+	flag.Parse()
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	
+	conn,err := grpc.Dial(*serverAddr,opts...)
+	if err != nil{
+		log.Fatalf("fail to dail:%v",err)
+	}
+	defer conn.Close()
+	client :=pb.NewEchoerClient(conn)
+	runEcho(client)
+}
